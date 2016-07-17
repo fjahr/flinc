42a913c56a6af4c37fae5c3929459f72ba36f51c
Fabian Jahr <fabian.jahr@whu.edu>
2016-07-18 00:57:52 +0200

    remove useless migrations

--- priv/repo/migrations/20160717212937_change_cards_position_to_float.exs
+++
@@ -1,9 +0,0 @@
-defmodule Flinc.Repo.Migrations.ChangeCardsPositionToFloat do
-  use Ecto.Migration
-
-  def change do
-    alter table(:cards) do
-      modify :position, :integer, default: 0
-    end
-  end
-end
